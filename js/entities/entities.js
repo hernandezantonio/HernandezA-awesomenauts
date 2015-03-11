7ba24467bc4f7a3e6a457c2dd12d0637fb006296
antonio hernandez <defygaming99@gmail.com>
2015-03-11 11:25:29 -0700

    2

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,7 @@ game.PlayerEntity = me.Entity.extend({
         }]); 
     
         this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
         
         this.renderable.addAnimation("idle", [78]); 
         this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
@@ -60,13 +61,20 @@ game.PlayerBaseEntity = me.Entity.extend({
           this.height =10; 
           this.alwaysUpdate = true;
           this.body.onCollision = this.onCollision.bind(this);
-          
+          console.log("init"); 
           this.type = "PlayerBaseEntity";
+          
+          this .renderable.addAnimation("idle", [0]);
+          this.renderable.addAnimation("broken", [1]);
+          this.renderable.setCurrentAnimation("idle"); 
+         
+          
       }, 
       
       update:function(delta){ 
           if(this.health<=0){
               this.broken = true;
+              this.renderable.setCurrentAnimation("broken"); 
   }
           this.body.update(delta);
           
@@ -78,3 +86,43 @@ game.PlayerBaseEntity = me.Entity.extend({
       }
       }); 
       
+      
+game.EnemyBaseEntity = me.Entity.extend({ 
+    init : function(x, y, settings){
+          this._super(me.Entity, 'init', [x, y,{
+              image: "tower",
+              width: 100, 
+              height: 100, 
+              spritewidth: "100",
+              spriteheight: "100", 
+              getShape: function(){
+                  return (new me.Rect(0, 0, 100, 100)).toPolygon();
+              }
+          }]); 
+          this.broken = false;
+          this.height =10; 
+          this.alwaysUpdate = true;
+          this.body.onCollision = this.onCollision.bind(this);
+          
+          this.type = "EnemyBaseEntity";
+          
+          this .renderable.addAnimation("idle", [0]);
+          this.renderable.addAnimation("broken", [1]);
+          this.renderable.setCurrentAnimation("idle"); 
+         
+      }, 
+      
+      update:function(delta){ 
+          if(this.health<=0){
+              this.broken = true;
+              this.renderable.setCurrentAnimation("broken"); 
+  }
+          this.body.update(delta);
+          
+          this._super(me.Entity, "update", [delta]);
+          return true;
+      }, 
+      onCollision: function(){
+          
+      }
+      }); 
