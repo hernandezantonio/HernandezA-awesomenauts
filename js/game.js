e740e3eb62b6c3ac2f58e5408c8a7656698f7ed6
antonio hernandez <defygaming99@gmail.com>
2015-04-23 10:52:54 -0700

    yup

--- js/game.js
+++ js/game.js
@@ -21,11 +21,19 @@ var game = {
                 player:"",
                 exp: 0,
                 gold: 0,
+                ability1: 0,
+                ability2: 0,
+                ability3: 0,
+                skill1: 0,
+                skill2: 0,
+                skill3: 0,
                 exp1: 0,
                 exp2: 0,
                 exp3: 0,
                 exp4: 0,
-                win:""
+                win:"",
+                pausePos: "",
+                buytext: ""
                 
                 
                 
@@ -71,6 +79,7 @@ var game = {
                 me.pool.register("GameTimerManager", game.GameTimerManager);
                 me.pool.register("HeroDeathManager", game.HeroDeathManager); 
                 me.pool.register("ExperienceManager", game.ExperienceManager); 
+                me.pool.register("SpendGold", game.SpendGold);
                 
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
