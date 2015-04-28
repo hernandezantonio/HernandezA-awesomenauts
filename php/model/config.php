3c262446817af29afbacf91a2ec40979fce0ec6c
antonio hernandez <defygaming99@gmail.com>
2015-04-28 11:25:43 -0700

    no message

--- php/model/config.php
+++ php/model/config.php
@@ -4,12 +4,12 @@ require_once(__DIR__ . "/database.php");
 session_start();
 session_regenerate_id(true); 
 
-$path = "/HernandezA-blog/";
+$path = "/AntonioHawesomenauts/php/";
 
 $host = "localhost";
 $username = "root";
 $password = "root";
-$database = "blog_db";
+$database = "awesomenauts_db";
 
 //this is asking if the connection is set 
 if (!isset($_SESSION["connection"])) {
